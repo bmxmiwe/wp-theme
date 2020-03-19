a2ded4283d49a7b72e99b84ae5d42ca439f8a980
adamdominik <adominik@sii.pl>
2020-03-19 15:44:50 +0100

    banks in macros are created

---
+++ resources/assets/scripts/macros/loans/banks.js
@@ -0,0 +1,413 @@
+export const BANKS = {
+    defaultValue: null,
+    component: {
+        label: 'Pankki',
+        type: 'DropDown',
+        options: [
+            {
+                value: 0,
+                label: 'Ej vald',
+            },
+            {
+                value: 1,
+                label: 'SBAB',
+            },
+            {
+                value: 2,
+                label: 'SEB',
+            },
+            {
+                value: 3,
+                label: 'Handelsbanken',
+            },
+            {
+                value: 4,
+                label: 'Nordea',
+            },
+            {
+                value: 5,
+                label: 'IKANO Banken',
+            },
+            {
+                value: 6,
+                label: 'ICA Banken',
+            },
+            {
+                value: 7,
+                label: 'Forex Bank',
+            },
+            {
+                value: 8,
+                label: 'Kaupthing Bank',
+            },
+            {
+                value: 10,
+                label: 'Sparbanken Öresund',
+            },
+            {
+                value: 11,
+                label: 'Swedbank',
+            },
+            {
+                value: 12,
+                label: 'Länsförsäkringar Bank',
+            },
+            {
+                value: 13,
+                label: 'Skandiabanken',
+            },
+            {
+                value: 14,
+                label: 'Danske Bank',
+            },
+            {
+                value: 15,
+                label: 'Marginalen Bank Bolån',
+            },
+            {
+                value: 22,
+                label: 'Fredell Loan',
+            },
+            {
+                value: 26,
+                label: 'HB Hypotek / Stadshypotek',
+            },
+            {
+                value: 29,
+                label: 'Handelsbanken Finans',
+            },
+            {
+                value: 31,
+                label: 'Nordea Hypotek',
+            },
+            {
+                value: 38,
+                label: 'SEB Bolån',
+            },
+            {
+                value: 40,
+                label: 'Sparbank - fristående',
+            },
+            {
+                value: 42,
+                label: 'Okobanken',
+            },
+            {
+                value: 43,
+                label: 'Landshypotek',
+            },
+            {
+                value: 52,
+                label: 'Danske Bolån',
+            },
+            {
+                value: 56,
+                label: 'LF Hypotek',
+            },
+            {
+                value: 58,
+                label: 'JAK Medlemsbank',
+            },
+            {
+                value: 64,
+                label: 'DnB NOR Bank',
+            },
+            {
+                value: 67,
+                label: 'Resurs Bank',
+            },
+            {
+                value: 69,
+                label: 'Konsument- / Privatkredit',
+            },
+            {
+                value: 75,
+                label: 'Kronofogden',
+            },
+            {
+                value: 76,
+                label: 'Trafikförsäkringsföreningen',
+            },
+            {
+                value: 77,
+                label: 'Blue Step',
+            },
+            {
+                value: 78,
+                label: 'Bostadsfinans',
+            },
+            {
+                value: 79,
+                label: 'Intrum Justitia',
+            },
+            {
+                value: 80,
+                label: 'Svea Ekonomi',
+            },
+            {
+                value: 81,
+                label: 'Svea Inkasso',
+            },
+            {
+                value: 82,
+                label: 'Reverskontroll',
+            },
+            {
+                value: 83,
+                label: 'Lindorff',
+            },
+            {
+                value: 85,
+                label: 'Gothia',
+            },
+            {
+                value: 86,
+                label: 'Sergel Inkasso',
+            },
+            {
+                value: 88,
+                label: 'Prioritet Inkasso',
+            },
+            {
+                value: 90,
+                label: 'Visma Collectors',
+            },
+            {
+                value: 92,
+                label: 'PayEx Finance',
+            },
+            {
+                value: 93,
+                label: 'Reda Inkasso',
+            },
+            {
+                value: 94,
+                label: 'Inga',
+            },
+            {
+                value: 95,
+                label: 'Finaref',
+            },
+            {
+                value: 96,
+                label: 'Nordax Finans',
+            },
+            {
+                value: 97,
+                label: 'Wasa Kredit',
+            },
+            {
+                value: 98,
+                label: 'CSN',
+            },
+            {
+                value: 99,
+                label: 'Haléns Finans',
+            },
+            {
+                value: 100,
+                label: 'Cash Buddy',
+            },
+            {
+                value: 102,
+                label: 'Bensinkort',
+            },
+            {
+                value: 103,
+                label: 'Finax',
+            },
+            {
+                value: 104,
+                label: 'Övr bolån',
+            },
+            {
+                value: 105,
+                label: 'Kreditkort',
+            },
+            {
+                value: 106,
+                label: 'Postorder',
+            },
+            {
+                value: 107,
+                label: 'Övr privatlån',
+            },
+            {
+                value: 109,
+                label: 'Provinsbanken',
+            },
+            {
+                value: 111,
+                label: 'SevenDay Finans',
+            },
+            {
+                value: 112,
+                label: 'Billån',
+            },
+            {
+                value: 113,
+                label: 'Santander',
+            },
+            {
+                value: 114,
+                label: 'Avanza Bank',
+            },
+            {
+                value: 119,
+                label: 'Volvo Finans',
+            },
+            {
+                value: 120,
+                label: 'SMS-lån',
+            },
+            {
+                value: 121,
+                label: 'Thorn',
+            },
+            {
+                value: 122,
+                label: 'Bohusbanken',
+            },
+            {
+                value: 123,
+                label: 'Collector',
+            },
+            {
+                value: 125,
+                label: 'Jotex',
+            },
+            {
+                value: 126,
+                label: 'Cellbes',
+            },
+            {
+                value: 127,
+                label: 'Klarna',
+            },
+            {
+                value: 128,
+                label: 'H&M',
+            },
+            {
+                value: 129,
+                label: 'Lindex',
+            },
+            {
+                value: 130,
+                label: 'Ellos',
+            },
+            {
+                value: 131,
+                label: 'Ikea Handla krediten',
+            },
+            {
+                value: 132,
+                label: 'ICA kort',
+            },
+            {
+                value: 133,
+                label: 'Time Finans',
+            },
+            {
+                value: 134,
+                label: 'Coop MedMera',
+            },
+            {
+                value: 135,
+                label: 'Ving',
+            },
+            {
+                value: 137,
+                label: 'La redoute',
+            },
+            {
+                value: 138,
+                label: 'Avida Finans',
+            },
+            {
+                value: 139,
+                label: 'Selene Finans',
+            },
+            {
+                value: 140,
+                label: 'Jysk',
+            },
+            {
+                value: 142,
+                label: 'Hemtex',
+            },
+            {
+                value: 144,
+                label: 'Medical Finance',
+            },
+            {
+                value: 146,
+                label: 'Cash 2 U',
+            },
+            {
+                value: 147,
+                label: 'Marginalen Bank',
+            },
+            {
+                value: 150,
+                label: 'Sparbanken Syd',
+            },
+            {
+                value: 151,
+                label: 'Nordnet Bank',
+            },
+            {
+                value: 152,
+                label: 'BIGBANK',
+            },
+            {
+                value: 153,
+                label: 'Inkasso',
+            },
+            {
+                value: 154,
+                label: 'Credex',
+            },
+            {
+                value: 155,
+                label: 'Credento',
+            },
+            {
+                value: 156,
+                label: 'Bank Norwegian',
+            },
+            {
+                value: 157,
+                label: 'Remember',
+            },
+            {
+                value: 158,
+                label: 'Sparbanken Skåne',
+            },
+            {
+                value: 159,
+                label: 'Spring Finance',
+            },
+            {
+                value: 160,
+                label: 'Villabanken',
+            },
+            {
+                value: 161,
+                label: 'Konsumentkredit',
+            },
+            {
+                value: 162,
+                label: 'MoneyGo',
+            },
+            {
+                value: 163,
+                label: 'Lån & Spar Bank',
+            },
+            {
+                value: 164,
+                label: 'OKQ8',
+            },
+        ],
+    },
+}
